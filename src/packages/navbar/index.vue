b75e144fee8802685e245f1659073b5968ef1b1f
“liqiong-lab” <1450805902@qq.com>
2020-11-19 15:40:22 +0800

    test: 测试上传

--- src/packages/navbar/index.vue
+++ src/packages/navbar/index.vue
@@ -1,18 +1,18 @@
 <template>
   <view :class="classes" @click="handleClick">
     <slot>
-      <!-- 左侧  icon-->
+      <!-- icon-->
       <view class="nut-navbar__left">
         <nut-icon v-if="leftShow" color="#979797" name="left"></nut-icon>
       </view>
 
-      <!-- 中间  title/icon/多个title-->
+      <!-- title/icon/多个title-->
       <view class="nut-navbar__title" :class="{ icon: icon }" v-if="title || titIcon">
         <view v-if="title">{{ title }}</view>
         <nut-icon v-if="titIcon" class="icon" :name="titIcon"></nut-icon>
       </view>
 
-      <!-- 右侧  title/icon/多个tit/多个icon-->
+      <!-- title/icon/多个tit/多个icon-->
       <view class="nut-navbar__right" :class="{ icon: icon }" v-if="desc || icon">
         <view v-if="desc" :style="{ 'text-align': descTextAlign }">{{ desc }}</view>
         <view> <nut-icon v-if="icon" class="icon" :name="icon"></nut-icon></view>
